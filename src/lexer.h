32fbfe704b08bdf60ca62f2a7f0c3100adda9ec4
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-08-25 03:21:32 +0900

    init lexer func

---
+++ src/lexer.h
@@ -0,0 +1,15 @@
+#include "include/lexer.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+lexer_T* init_lexer(char* contents) // init lexer.
+{
+    lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
+    lexer->contents = contents;
+    lexer->i = 0;
+    lexer->c = contents[lexer->i];
+
+    return lexer;
+}

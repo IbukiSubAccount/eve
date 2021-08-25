586b1e4ad157465eed1f6153ba0bec46f70140f3
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-08-26 04:54:48 +0900

    parser.c init parser func

---
+++ src/parser.c
@@ -0,0 +1,30 @@
+#include "include/parser.h"
+
+parser_T* init_parser(lexer_T* lexer)
+{
+    parser_T* parser = calloc(1, sizeof(struct PARSER_STRUCT));
+    parser->lexer = lexer;
+    parser->current_token = lexer_get_next_token(lexer);
+
+    return parser;
+}
+
+void parser_eat(parser_T* parser, int token_type); // get a expected token
+
+AST_T* parser_parse(parser_T* parser); // main entry point of the parser. return the whole AST tree of the entire source code when we parse.
+
+AST_T* parser_parse_statement(parser_T* parser); // parsing single statement
+
+AST_T* parser_parse_statements(parser_T* parser); // parsing list of statement
+
+AST_T* parser_parse_expr(parser_T* parser);
+
+AST_T* parser_parse_factor(parser_T* parser);
+
+AST_T* parser_parse_term(parser_T* parser);
+
+AST_T* parser_parse_function_call(parser_T* parser); // return AST node type of function call
+
+AST_T* parser_parse_variable(parser_T* parser); // return AST node type of variable
+
+AST_T* parser_parse_string(parser_T* parser); // return AST node type of string

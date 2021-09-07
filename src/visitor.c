9a943a144d0831d79ce8441ea368cd23aed14d68
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-07 11:29:18 +0900

    Eve v1.5.1 edited error message & debug function definiton

--- src/visitor.c
+++ src/visitor.c
@@ -32,7 +32,7 @@ static AST_T* builtin_function_sleep(visitor_T* visitor, AST_T** args, int args_
         switch (visited_ast->type)
         {
             case AST_INT: sleep(visited_ast->int_value); break;
-            default: printf("Sleep function data type error expecting integer\n"); exit(1);
+            default: printf("[ERROR] Sleep function expecting integer\n"); exit(1);
         }
     }
 
@@ -60,7 +60,7 @@ AST_T* visitor_visit(visitor_T* visitor, AST_T* node)
         case AST_NOOP: return node; break;
     }
 
-    printf("Uncaught statement of type '%d'\n", node->type);
+    printf("[ERROR] Uncaught statement of type '%d'\n", node->type);
     exit(1);
 
     return init_ast(AST_NOOP);
@@ -97,7 +97,7 @@ AST_T* visitor_visit_variable(visitor_T* visitor, AST_T* node)
     if (vdef != (void*) 0)
         return visitor_visit(visitor, vdef->variable_definition_value);
 
-    printf("Undifined variable %s\n", node->variable_name);
+    printf("[ERROR] Undifined variable %s\n", node->variable_name);
     exit(1);
 }
 
@@ -123,7 +123,7 @@ AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node)
 
     if (fdef == (void*)0)
     {
-        printf("Undifiend method '%s'\n", node->function_call_name);
+        printf("[ERROR] Undifiend method '%s'\n", node->function_call_name);
         exit(1);
     }
 

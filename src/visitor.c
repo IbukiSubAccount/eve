b0ac42e96e9d96e3bc1165081b8218ac1f49dd16
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-09 18:22:24 +0900

    edited error message

--- src/visitor.c
+++ src/visitor.c
@@ -32,7 +32,7 @@ static AST_T* builtin_function_sleep(visitor_T* visitor, AST_T** args, int args_
         switch (visited_ast->type)
         {
             case AST_INT: sleep(visited_ast->int_value); break;
-            default: printf("Error Sleep function expecting integer\n"); exit(1);
+            default: printf("\x1b[31m");printf("Error Sleep function expecting integer\n"); exit(1);
         }
     }
 
@@ -60,6 +60,7 @@ AST_T* visitor_visit(visitor_T* visitor, AST_T* node)
         case AST_NOOP: return node; break;
     }
 
+    printf("\x1b[31m");
     printf("Error: Uncaught statement of type '%d'\n", node->type);
     exit(1);
 
@@ -96,7 +97,8 @@ AST_T* visitor_visit_variable(visitor_T* visitor, AST_T* node)
 
     if (vdef != (void*) 0)
         return visitor_visit(visitor, vdef->variable_definition_value);
-
+    
+    printf("\x1b[31m");
     printf("Error: Undifined variable '%s'\n", node->variable_name);
     exit(1);
 }
@@ -123,6 +125,7 @@ AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node)
 
     if (fdef == (void*)0)
     {
+        printf("\x1b[31m");
         printf("Error: Undifiend method '%s'\n", node->function_call_name);
         exit(1);
     }

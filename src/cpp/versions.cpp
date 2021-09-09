bc2d85ff2b4ec33816e6e9731a2af99b703f9339
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-09 18:14:43 +0900

    output versions

---
+++ src/cpp/versions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "\033[32mEve install 100% done \033[m✨🚀"<< std::endl;
+
+    // std::cout << "GitHub： https://github.com/ibukiyoshidaa/eve/blob/main/README.md"<< std::endl;
+
+    std::cout << "How to use： https://eveofficial.herokuapp.com/docs"<< std::endl;
+
+    std::cout << ">> Eve Version 1.7.0"<< std::endl;
+
+    return 0;
+}

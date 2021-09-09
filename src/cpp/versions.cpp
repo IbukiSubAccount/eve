864b160c1345b362add58f3dd2db51af6aba2aff
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-09 20:10:13 +0900

    edited versions.cpp added report bug url

--- src/cpp/versions.cpp
+++ src/cpp/versions.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
+// this will run when sudo make install
 int main() {
-    
+
     std::cout << "\033[32mEve install 100% done \033[m✨🚀"<< std::endl;
 
     // std::cout << "GitHub： https://github.com/ibukiyoshidaa/eve/blob/main/README.md"<< std::endl;
 
-    std::cout << "How to use： https://eveofficial.herokuapp.com/docs"<< std::endl;
+    std::cout << "How to use 🧐：https://eveofficial.herokuapp.com/docs"<< std::endl;
+    std::cout << "Report a Bug 😝：https://github.com/ibukiyoshidaa/eve/issues"<< std::endl;
 
-    std::cout << ">> Eve Version 1.7.0"<< std::endl;
+    std::cout << ">> Eve Version 1.7.0 😆"<< std::endl;
 
     return 0;
 }

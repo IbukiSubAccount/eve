14798faf5bb9d20bd5c9408af75ab4ca71e36b44
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-18 18:31:23 +0900

    added scope add list & get list

--- src/include/scope.h
+++ src/include/scope.h
@@ -9,6 +9,9 @@ typedef struct SCOPE_STRUCT
 
     AST_T** variable_definitions;
     size_t variable_definitions_size;
+
+    AST_T** list_definitions;
+    size_t list_definitions_size;
 } scope_T;
 
 scope_T* init_scope();
@@ -20,4 +23,8 @@ AST_T* scope_get_function_definition(scope_T* scope, const char* fname);
 AST_T* scope_add_variable_definition(scope_T* scope, AST_T* vdef);
 
 AST_T* scope_get_variable_definition(scope_T* scope, const char* name);
+
+AST_T* scope_add_list_definition(scope_T* scope, AST_T* ldef);
+
+AST_T* scope_get_list_definition(scope_T* scope, const char* lname);
 #endif

fe2e8d73bd52120c06e656e3bd5febce7f480f2c
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-18 18:33:33 +0900

    edited comments & added visitor visit list & visitor visit list

--- src/include/visitor.h
+++ src/include/visitor.h
@@ -7,27 +7,26 @@ typedef struct VISITOR_STRUCT
     
 } visitor_T;
 
-/* for this program
-v name = "Ibuki Yoshida";
-eve(name);
-*/
-
 visitor_T* init_visitor();
 
-AST_T* visitor_visit(visitor_T* visitor, AST_T* node); // 1. take in the root node and check the types and visit
+AST_T* visitor_visit(visitor_T* visitor, AST_T* node); // take in the root node and check the types and visit
 
-AST_T* visitor_visit_variable_definition(visitor_T* visitor, AST_T* node); // 3. visit the variabe definition
+AST_T* visitor_visit_variable_definition(visitor_T* visitor, AST_T* node); // visit the variabe definition
 
 AST_T* visitor_visit_function_definition(visitor_T* visitor, AST_T* node);
 
-AST_T* visitor_visit_variable(visitor_T* visitor, AST_T* node); // 5. visit the variable and look up the memory
+AST_T* visitor_visit_variable(visitor_T* visitor, AST_T* node); // visit the variable and look up the memory
 
-AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node); // 4. visit function call
+AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node); // visit function call
+
+AST_T* visitor_visit_list_definition(visitor_T* visitor, AST_T* node); // visit list definition
+
+AST_T* visitor_visit_list(visitor_T* visitor, AST_T* node); // visit list and look up the memory
 
 AST_T* visitor_visit_string(visitor_T* visitor, AST_T* node);
 
 AST_T* visitor_visit_int(visitor_T* visitor, AST_T* node);
 
-AST_T* visitor_visit_compound(visitor_T* visitor, AST_T* node); // 2. loop the compound children
+AST_T* visitor_visit_compound(visitor_T* visitor, AST_T* node); // loop the compound children
 
 #endif

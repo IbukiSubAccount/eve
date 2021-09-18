912496fac662482952feb9305e965d3a5d924b11
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-09-18 18:32:46 +0900

    added scope get list & scope add list

--- src/scope.c
+++ src/scope.c
@@ -1,5 +1,6 @@
 #include "include/scope.h"
 #include <string.h>
+#include <stdio.h>
 
 scope_T* init_scope()
 {
@@ -9,6 +10,9 @@ scope_T* init_scope()
 
     scope->variable_definitions = (void*) 0;
     scope->variable_definitions_size = 0;
+
+    scope->list_definitions = (void*) 0;
+    scope->list_definitions_size = 0;
 
     return scope;
 }
@@ -78,6 +82,43 @@ AST_T* scope_get_variable_definition(scope_T* scope, const char* name)
         if (strcmp(vdef->variable_definition_variable_name, name) == 0)
         {
             return vdef;
+        }
+    }
+
+    return (void*)0;
+}
+
+AST_T* scope_add_list_definition(scope_T* scope, AST_T* ldef)
+{
+    scope->list_definitions_size += 1;
+
+    if (scope->list_definitions == (void*) 0)
+    {
+        scope->list_definitions = calloc(1, sizeof(struct AST_STRUCT*));
+    }
+    else
+    {
+        scope->list_definitions = 
+            realloc(
+                scope->list_definitions,
+                scope->list_definitions_size * sizeof(struct AST_STRUCT**)
+            );
+    }
+
+    scope->list_definitions[scope->list_definitions_size-1] = ldef;
+
+    return ldef;
+}
+
+
+AST_T* scope_get_list_definition(scope_T* scope, const char* lname)
+{
+    for (int i = 0; i < scope->list_definitions_size; i++)
+    {
+        AST_T* ldef = scope->list_definitions[i];
+        if (strcmp(ldef->list_definition_name, lname) == 0)
+        {
+            return ldef;
         }
     }
 
